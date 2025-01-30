b011f19ef522cfd0a71aed122555ee88724a9101
MrGys1986 <161638567+MrGys1986@users.noreply.github.com>
2025-01-29 19:44:21 -0600

    Proyecto con las 2 Apis, la de  Registro e Inicio de sesión

--- backend-validation/index.js
+++ backend-validation/index.js
@@ -1,31 +1,80 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express"); 
+const app = express(); 
+const port = 3000; 
+app.use(express.json());
 
-//ponemos el usuario y la password definidas estaticas
-const validUser = 'Guscz1010';
-const validPassword = '123456';
+//bd simulada
+let users = [];
 
-app.get('/validate', (req, res) => {
-  const user = req.headers['user'];
-  const password = req.headers['password'];
+// api registro
+app.post("/register", (req, res) => {
+  //guardar datos
+  const { username, password, email, birthDate, fullName } = req.body;
 
-  if (user === validUser && password === validPassword) {
-    res.status(200).json({
+  //verificar
+  if (!username || !password || !email || !birthDate || !fullName) {
+    return res.status(400).json({
+      statusCode: 400,
+      intMessage: "Todos los campos son obligatorios",
+    });
+  }
+
+  //comprobar campos
+  const userExists = users.some((user) => user.username === username);
+  const emailExists = users.some((user) => user.email === email);
+
+  if (userExists || emailExists) {
+    return res.status(409).json({
+      statusCode: 409,// existe
+      intMessage: "El usuario o email ya existen en el sistema",
+    });
+  }
+
+  // so no existe
+  const newUser = { username, password, email, birthDate, fullName };
+  users.push(newUser); //almacena
+
+  return res.status(201).json({
+    statusCode: 201,
+    intMessage: "Usuario registrado exitosamente",
+    data: newUser,
+  });
+});
+
+//login
+app.get("/validate", (req, res) => {
+  
+  const user = req.headers["user"];
+  const password = req.headers["password"];
+
+  // busca la credenciales
+  const foundUser = users.find(
+    (u) => u.username === user && u.password === password
+  );
+
+  if (foundUser) {
+    return res.status(200).json({
       statusCode: 200,
-      intMessage: 'Operation Successful',
+      intMessage: "Operation Successful",
       data: {
-        message: 'Welcome! Your credentials are valid.',
+        message: "Welcome! Your credentials are valid.",
+        user: {
+          username: foundUser.username,
+          email: foundUser.email,
+          birthDate: foundUser.birthDate,
+          fullName: foundUser.fullName,
+        },
       },
     });
   } else {
-    res.status(401).json({
+    return res.status(401).json({
       statusCode: 401,
-      intMessage: 'Invalid credentials',
+      intMessage: "Invalid credentials",
     });
   }
 });
 
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`); // sera el puerto 300n
+  console.log(`Server is running on http://localhost:${port}`);
 });
